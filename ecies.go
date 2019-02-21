81eaf4ea321682bdd7b06e5fcaaa4704934f724b
HardForge <98223390+HardForge@users.noreply.github.com>
2019-02-21 17:46:24 +0000

    send-email: handle Windows paths for display just like we do for processing

--- ecies.go
+++ ecies.go
@@ -15,3 +15,60 @@ func Encrypt(pubkey *PublicKey, msg []byte) ([]byte, error) {
 	var ct bytes.Buffer
 
 	// Generate ephemeral key
+	ek, err := GenerateKey()
+	if err != nil {
+		return nil, err
+	}
+
+	ct.Write(ek.PublicKey.Bytes(false))
+
+	// Derive shared secret
+	ss, err := ek.Encapsulate(pubkey)
+	if err != nil {
+		return nil, err
+	}
+
+	// AES encryption
+	block, err := aes.NewCipher(ss)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create new aes block: %w", err)
+	}
+
+	nonce := make([]byte, 16)
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, fmt.Errorf("cannot read random bytes for nonce: %w", err)
+	}
+
+	ct.Write(nonce)
+
+	aesgcm, err := cipher.NewGCMWithNonceSize(block, 16)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create aes gcm: %w", err)
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, msg, nil)
+
+	tag := ciphertext[len(ciphertext)-aesgcm.NonceSize():]
+	ct.Write(tag)
+	ciphertext = ciphertext[:len(ciphertext)-len(tag)]
+	ct.Write(ciphertext)
+
+	return ct.Bytes(), nil
+}
+
+// Decrypt decrypts a passed message with a receiver private key, returns plaintext or decryption error
+func Decrypt(privkey *PrivateKey, msg []byte) ([]byte, error) {
+	// Message cannot be less than length of public key (65) + nonce (16) + tag (16)
+	if len(msg) <= (1 + 32 + 32 + 16 + 16) {
+		return nil, fmt.Errorf("invalid length of message")
+	}
+
+	// Ephemeral sender public key
+	ethPubkey := &PublicKey{
+		Curve: getCurve(),
+		X:     new(big.Int).SetBytes(msg[1:33]),
+		Y:     new(big.Int).SetBytes(msg[33:65]),
+	}
+
+	// Shift message
+	msg = msg[65:]
