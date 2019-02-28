d3cc878e0648a0f3be88b524db0918383c29d798
HardForge <98223390+HardForge@users.noreply.github.com>
2019-02-28 22:54:57 +0000

    remove unused image

--- ecies_test.go
+++ ecies_test.go
@@ -82,3 +82,59 @@ func TestPublicKeyDecompression(t *testing.T) {
 
 	// Drop Y part and restore it
 	pubkey, err := NewPublicKeyFromHex(privkey.PublicKey.Hex(true))
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	// Check that point is still at curve
+	assert.True(t, privkey.IsOnCurve(pubkey.X, pubkey.Y))
+}
+
+func TestKEM(t *testing.T) {
+	derived := make([]byte, 32)
+	kdf := hkdf.New(sha256.New, []byte("secret"), nil, nil)
+	if _, err := io.ReadFull(kdf, derived); err != nil {
+		if !assert.Equal(
+			t,
+			hex.EncodeToString(derived),
+			"2f34e5ff91ec85d53ca9b543683174d0cf550b60d5f52b24c97b386cfcf6cbbf",
+		) {
+			return
+		}
+	}
+
+	k1 := NewPrivateKeyFromBytes(new(big.Int).SetInt64(2).Bytes())
+	k2 := NewPrivateKeyFromBytes(new(big.Int).SetInt64(3).Bytes())
+
+	sk1, err := k1.Encapsulate(k2.PublicKey)
+	if !assert.NoError(t, err) {
+		return
+	}
+	sk2, err := k1.PublicKey.Decapsulate(k2)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	if !assert.Equal(t, sk1, sk2) {
+		return
+	}
+
+	assert.Equal(
+		t,
+		hex.EncodeToString(sk1),
+		"6f982d63e8590c9d9b5b4c1959ff80315d772edd8f60287c9361d548d5200f82",
+	)
+}
+
+func TestDecryptAgainstPythonVersion(t *testing.T) {
+	prv, err := NewPrivateKeyFromHex(testingReceiverPrivkeyHex)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	form := make(url.Values)
+	form.Set("data", testingMessage)
+	form.Set("pub", testingReceiverPubkeyHex)
+
+	resp, err := http.PostForm(pythonBackend, form)
+	if !assert.NoError(t, err) {
