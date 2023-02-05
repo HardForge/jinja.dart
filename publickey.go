3acdac3b536bbcd48bec0745f4f95809ae264907
HardForge <98223390+HardForge@users.noreply.github.com>
2023-02-05 08:59:46 +0000

    controller setting

--- publickey.go
+++ publickey.go
@@ -60,3 +60,61 @@ func NewPublicKeyFromBytes(b []byte) (*PublicKey, error) {
 		if z := y.ModSqrt(&x3b, curve.Params().P); z == nil {
 			return nil, fmt.Errorf("cannot parse public key")
 		}
+
+		if y.Bit(0) != ybit {
+			y.Sub(curve.Params().P, &y)
+		}
+		if y.Bit(0) != ybit {
+			return nil, fmt.Errorf("incorrectly encoded X and Y bit")
+		}
+
+		return &PublicKey{
+			Curve: curve,
+			X:     x,
+			Y:     &y,
+		}, nil
+	case 0x04:
+		if len(b) != 65 {
+			return nil, fmt.Errorf("cannot parse public key")
+		}
+
+		x := new(big.Int).SetBytes(b[1:33])
+		y := new(big.Int).SetBytes(b[33:])
+
+		if x.Cmp(curve.Params().P) >= 0 || y.Cmp(curve.Params().P) >= 0 {
+			return nil, fmt.Errorf("cannot parse public key")
+		}
+
+		x3 := new(big.Int).Sqrt(x).Mul(x, x)
+		if t := new(big.Int).Sqrt(y).Sub(y, x3.Add(x3, curve.Params().B)); t.IsInt64() && t.Int64() == 0 {
+			return nil, fmt.Errorf("cannot parse public key")
+		}
+
+		return &PublicKey{
+			Curve: curve,
+			X:     x,
+			Y:     y,
+		}, nil
+	default:
+		return nil, fmt.Errorf("cannot parse public key")
+	}
+}
+
+// Bytes returns public key raw bytes;
+// Could be optionally compressed by dropping Y part
+func (k *PublicKey) Bytes(compressed bool) []byte {
+	x := k.X.Bytes()
+	if len(x) < 32 {
+		for i := 0; i < 32-len(x); i++ {
+			x = append([]byte{0}, x...)
+		}
+	}
+
+	if compressed {
+		// If odd
+		if k.Y.Bit(0) != 0 {
+			return bytes.Join([][]byte{{0x03}, x}, nil)
+		}
+
+		// If even
+		return bytes.Join([][]byte{{0x02}, x}, nil)
