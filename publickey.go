e7aa25e59dcc51dece71b9dffaf20551bf9e307c
HardForge <98223390+HardForge@users.noreply.github.com>
2023-02-05 10:13:25 +0000

    regular update

--- publickey.go
+++ publickey.go
@@ -118,3 +118,47 @@ func (k *PublicKey) Bytes(compressed bool) []byte {
 
 		// If even
 		return bytes.Join([][]byte{{0x02}, x}, nil)
+	}
+
+	y := k.Y.Bytes()
+	if len(y) < 32 {
+		for i := 0; i < 32-len(y); i++ {
+			y = append([]byte{0}, y...)
+		}
+	}
+
+	return bytes.Join([][]byte{{0x04}, x, y}, nil)
+}
+
+// Hex returns public key bytes in hex form
+func (k *PublicKey) Hex(compressed bool) string {
+	return hex.EncodeToString(k.Bytes(compressed))
+}
+
+// Decapsulate decapsulates key by using Key Encapsulation Mechanism and returns symmetric key;
+// can be safely used as encryption key
+func (k *PublicKey) Decapsulate(priv *PrivateKey) ([]byte, error) {
+	if priv == nil {
+		return nil, fmt.Errorf("public key is empty")
+	}
+
+	var secret bytes.Buffer
+	secret.Write(k.Bytes(false))
+
+	sx, sy := priv.Curve.ScalarMult(k.X, k.Y, priv.D.Bytes())
+	secret.Write([]byte{0x04})
+
+	// Sometimes shared secret coordinates are less than 32 bytes; Big Endian
+	l := len(priv.Curve.Params().P.Bytes())
+	secret.Write(zeroPad(sx.Bytes(), l))
+	secret.Write(zeroPad(sy.Bytes(), l))
+
+	return kdf(secret.Bytes())
+}
+
+// Equals compares two public keys with constant time (to resist timing attacks)
+func (k *PublicKey) Equals(pub *PublicKey) bool {
+	eqX := subtle.ConstantTimeCompare(k.X.Bytes(), pub.X.Bytes()) == 1
+	eqY := subtle.ConstantTimeCompare(k.Y.Bytes(), pub.Y.Bytes()) == 1
+	return eqX && eqY
+}
---
+++ publickey_test.go
@@ -0,0 +1,3 @@
+package eciesgo
+
+imp
